e9b2be10e8f2984ec1f060f5a81a2d02de18189b
Thomas LOSBAR <thomas.losbar@gmail.com>
2015-05-06 21:24:26 +0200

    integration activity searchcard en main

---
+++ Sportify/app/src/main/java/com/pld/h4414/sportify/SearchOptionsFragment.java
@@ -0,0 +1,78 @@
+package com.pld.h4414.sportify;
+
+import android.app.Activity;
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnFragmentInteractionListener}
+ * interface.
+ */
+public class SearchOptionsFragment extends ListFragment {
+    private String[] mTypeSearch;
+    private OnFragmentInteractionListener mListener;
+
+    public SearchOptionsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        mTypeSearch = getResources().getStringArray(R.array.type_search_array);
+        super.onCreate(savedInstanceState);
+
+        setListAdapter(new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, mTypeSearch));
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        if (null != mListener) {
+            // Notify the active callbacks interface (the activity, if the
+            // fragment is attached to one) that an item has been selected.
+            String sTypeSearch = getResources().getStringArray(R.array.type_search_array)[position];
+            mListener.onFragmentInteraction(sTypeSearch);
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        public void onFragmentInteraction(String id);
+    }
+
+}
